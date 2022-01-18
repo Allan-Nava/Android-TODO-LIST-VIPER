591c86c8f28874ead8e0686df16f95b492a81abe
Allan Nava <allannava95@gmail.com>
2022-01-18 21:57:12 +0100

    added main module

---
+++ app/src/main/java/com/allannava/todolistviper/MainModule/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.allannava.todolistviper.MainModule
+
+/**
+ * Created by Allan Nava on 18/01/2022.
+ * Updated by Allan Nava on 18/01/2022.
+ */
+class MainActivity {
+
+}
---
+++ app/src/main/java/com/allannava/todolistviper/MainModule/MainInteractor.kt
@@ -0,0 +1,6 @@
+package com.allannava.todolistviper.MainModule
+
+/**
+ * Created by Allan Nava on 18/01/2022.
+ * Updated by Allan Nava on 18/01/2022.
+ */
