33cd8a62b6551fa5c653c4081e405113260dd1bb
Allan Nava <allannava95@gmail.com>
2022-01-19 14:08:34 +0100

    Update Todo.kt

--- app/src/main/java/com/allannava/todolistviper/Models/Todo.kt
+++ app/src/main/java/com/allannava/todolistviper/Models/Todo.kt
@@ -2,12 +2,13 @@ package com.allannava.todolistviper.Models
 
 /**
  * Created by Allan Nava on 18/01/2022.
- * Updated by Allan Nava on 18/01/2022.
+ * Updated by Allan Nava on 19/01/2022.
  */
 class Todo {
     //
     var id : Int? = null
     var name : String = ""
+    var description : String = ""
     //
     constructor(id: Int, name: String){
         this.id = id
